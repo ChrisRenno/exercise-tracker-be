7b9c17343f1477289cc9020e28b81f5593fd819c
ChrisRenno <cpreynolds@outlook.com>
2024-12-20 18:51:46 +0000

    getWorkoutsById working

---
+++ README.md
@@ -0,0 +1,11 @@
+Backend fuctions for the training weak project
+
+To deploy
+
+```firebase deploy``` from the functions folder
+
+Auth issues on deploy
+
+```firebase logout``` then ```firebase login```
+or
+```firebase --reauth```
--- functions/index.js
+++ functions/index.js
@@ -95,6 +95,14 @@ exports.getJsonFromDb = functions.https.onRequest(async (req, res) => {
 
 // Function to retrieve all workouts with a specific user id from Firestore
 exports.getWorkouts = functions.https.onRequest(async (req, res) => {
+  if (req.method === "OPTIONS") {
+    // Handle preflight request
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Methods", "GET, POST");
+    res.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.status(204).send("");
+    return;
+  }
   try {
     const snapshot = await db.collection("workouts").get();
     const data = snapshot.docs.map((doc) => doc.data());
@@ -103,20 +111,30 @@ exports.getWorkouts = functions.https.onRequest(async (req, res) => {
     res.status(500).send(error.toString());
   }
 });
-exports.getWorkoutsByUserId = functions.https.onRequest(async (req, res) => {
-  try {
-    const userId = req.query.userId;
-    if (!userId) {
-      return res.status(400).send("User ID is required");
+exports.getWorkoutsByUserId = functions.https.onRequest((req, res) => {
+  cors(corsOptions)(req, res, async () => {
+    if (req.method === "OPTIONS") {
+      // Handle preflight request
+      res.set("Access-Control-Allow-Origin", "*");
+      res.set("Access-Control-Allow-Methods", "GET, POST");
+      res.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      res.status(204).send("");
+      return;
     }
+    try {
+      const userId = req.query.userId;
+      if (!userId) {
+        return res.status(400).send("User ID is required");
+      }
 
-    const snapshot = await db.collection("workouts")
-        .where("userId", "==", userId).get();
-    const data = snapshot.docs.map((doc) => doc.data());
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).send(error.toString());
-  }
+      const snapshot = await db.collection("workouts")
+          .where("userID", "==", userId).get();
+      const data = snapshot.docs.map((doc) => doc.data());
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).send(error.toString());
+    }
+  });
 });
 
 
